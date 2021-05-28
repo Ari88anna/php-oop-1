7d5f28c860e7b430911d2db256268668d1e1c213
Arianna Carotti <arianna.carotti@gmail.com>
2021-05-28 15:29:18 +0200

    provata funzione

--- index.php
+++ index.php
@@ -12,19 +12,34 @@
 
         public $citta;
 
-        public $password;      
+        public $password;
+
+        public function nomeCompleto() {
+            return $this ->nome . ' ' . $this->cognome;
+        }      
 
     }
 
-    $frodo = new User();
-    $frodo -> nome = 'Frodo';    
+    $frodo = new User(); 
+    $frodo -> nome = 'Frodo';
     $frodo -> cognome = 'Baggins';
     $frodo -> username = 'frodoBB';
     $frodo -> mail = 'frodo.baggins@gmail.com';
     $frodo -> citta = 'Contea';
-    
 
+    echo $frodo ->nomeCompleto('Frodo', 'Baggins');
     var_dump($frodo);
+
+
+    $han = new User();
+    $han -> nome = 'Han';    
+    $han -> cognome = 'Solo';
+    $han -> username = 'hanSolo';
+    $han -> mail = 'han.solo@gmail.com';
+    $han -> citta = 'Corelia';
+    
+    echo $han ->nomeCompleto('Han', 'Solo');
+    var_dump($han);
 
 
 
