c08b8bdca1eace30ef907618eb9a9c6545f759a8
Arianna Carotti <arianna.carotti@gmail.com>
2021-05-28 15:43:29 +0200

    provata funzione construct

--- index.php
+++ index.php
@@ -14,33 +14,38 @@
 
         public $password;
 
-        public function nomeCompleto() {
-            return $this ->nome . ' ' . $this->cognome;
-        }      
+        // public function nomeCompleto() {
+        //     return $this ->nome . ' ' . $this->cognome;
+        // }      
+
+        public function __construct($nome, $cognome, $mail) {
+            $this ->nome = $nome; 
+            $this->cognome = $cognome;
+            $this ->email = $mail;             
+        } 
 
     }
 
-    $frodo = new User(); 
-    $frodo -> nome = 'Frodo';
-    $frodo -> cognome = 'Baggins';
+    $frodo = new User('Frodo', 'Baggins', 'frodo.baggins@gmail.com' ); 
+    // $frodo -> nome = 'Frodo';
+    // $frodo -> cognome = 'Baggins';
     $frodo -> username = 'frodoBB';
-    $frodo -> mail = 'frodo.baggins@gmail.com';
+    // $frodo -> mail = 'frodo.baggins@gmail.com';
     $frodo -> citta = 'Contea';
 
-    echo $frodo ->nomeCompleto('Frodo', 'Baggins');
+    // echo $frodo ->nomeCompleto('Frodo', 'Baggins');
     var_dump($frodo);
 
 
-    $han = new User();
-    $han -> nome = 'Han';    
-    $han -> cognome = 'Solo';
+    $han = new User('Han', 'Solo', 'han.solo@gmail.com');
+    // $han -> nome = 'Han';    
+    // $han -> cognome = 'Solo';
     $han -> username = 'hanSolo';
-    $han -> mail = 'han.solo@gmail.com';
+    // $han -> mail = 'han.solo@gmail.com';
     $han -> citta = 'Corelia';
     
-    echo $han ->nomeCompleto('Han', 'Solo');
+    // echo $han ->nomeCompleto('Han', 'Solo');
     var_dump($han);
 
-
-
+    
 ?>
